2bca3eaf6af2cc43053ffaf1f037dc16f91eac6d
Ben Strutt <ben.strutt1@nhs.net>
2023-02-10 00:36:47 +0000

    APM-3954 Add sandbox deployment

--- test/test_deployment.py
+++ test/test_deployment.py
@@ -2,10 +2,11 @@
 Example tests that show all the features of pytest_nhsd_apim.
 """
 import json
+from datetime import datetime, timedelta
+
 import pytest
 import requests
 
-from pytest_nhsd_apim import pytest_nhsd_apim
 
 def test_ping_endpoint(nhsd_apim_proxy_url):
     """
@@ -29,9 +30,28 @@ def test_status_endpoint(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     # {"apikey": "thesecretvalue"} Use it to access your proxy's
     # _status endpoint.
 
-    resp = requests.get(
-        nhsd_apim_proxy_url + "/_status", headers=status_endpoint_auth_headers
-    )
-    status_json = resp.json()
+    # Initial deployment to sandbox environments will return before
+    # the container is deployed.
+    start = datetime.now()
+
+    # AWS returns 503 when containers aren't ready.
+    status_json = {
+        "status": "fail",
+        "checks": {
+            "healthcheck": {
+                "status": "fail",
+                "responseCode": 503,
+            }
+        },
+    }
+    max_wait = timedelta(minutes=5)
+    while (
+        status_json["checks"]["healthcheck"]["responseCode"] == 503
+        and datetime.now() - start < max_wait
+    ):
+        resp = requests.get(
+            nhsd_apim_proxy_url + "/_status", headers=status_endpoint_auth_headers
+        )
+        status_json = resp.json()
     assert resp.status_code == 200
     assert status_json["status"] == "pass"
